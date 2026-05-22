246f20f22c9cd919d9e34861370849a5c3ff2792
Deenakaran1985 <innovcomp@gmail.com>
2026-05-22 18:05:32 +0530

    Fix: Add parentheses to versionCode and versionName in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,8 +26,9 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        // FIX: Add parentheses here - these are function calls
+        versionCode = flutter.versionCode()
+        versionName = flutter.versionName()
     }
 
     buildTypes {
